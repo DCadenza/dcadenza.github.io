5cbbd85214960c1db4848ef26f5830d451eaf7c9
DCadenza <dimagolubtsov@MacBook-Pro-Dima.local>
2017-07-22 01:17:45 +0300

    Initial commit
    Update

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -4,10 +4,3 @@ $(function() {
      return false;
    });
  });
-
-jQuery(document).ready(function() {
-jQuery('.post').addClass("hidden").viewportChecker({
-classToAdd: 'visible animated fadeIn',
-offset: 100
-});
-});
