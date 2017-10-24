a199599ed25a150ea38ad88766a1bdded1e38e31
DCadenza <dimagolubtsov@MacBook-Pro-Dima.local>
2017-10-24 11:30:38 +0300

    Update 24.10.17

---
+++ submit.php
@@ -0,0 +1,22 @@
+<?php
+if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['message']) AND !empty($_POST['phone'])){
+
+    $headers = 'From: Кротов Роман'
+                'Replay-To: '
+                'X-Meiler: PHP/'. phpversion();
+
+    $theme = 'Новое сообщение';
+
+    $letter = 'Данные сообщения';
+    $letter .= 'Имя: '.$_POST['name'].'\r\n';
+    $letter .= 'Email: '.$_POST['email'].'\r\n';
+    $letter .= 'Телефон: '.$_POST['phone'].'\r\n';
+    $letter .= 'Сообщения: '.$_POST['message'].'\r\n';
+    mail('dmitriiholubtsov@gmail.com', $theme, $letter);
+    if (mail('dmitriiholubtsov@gmail.com', $theme, $letter, $headers)){
+        header('Location:/thankyou.html')
+    }
+} else {
+    header('Location:/')
+}
+ ?>
