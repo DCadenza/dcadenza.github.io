7ec080549256f17b6f2c89359f74e17030f44fa1
DCadenza <dimagolubtsov@MacBook-Pro-Dima.local>
2018-02-18 00:08:12 +0200

    Init
    Upload submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,3 @@
-// // Contact form
-// $(document).ready(function() {
-//
-// 	//E-mail Ajax Send
-// 	$("form").submit(function() { //Change
-// 		var th = $(this);
-// 		$.ajax({
-// 			type: "POST",
-// 			url: "../mail.php", //Change
-// 			data: th.serialize()
-// 		}).done(function() {
-// 			alert("Thank you!");
-// 			setTimeout(function() {
-// 				// Done Functions
-// 				th.trigger("reset");
-// 			}, 1000);
-// 		});
-// 		return false;
-// 	});
-//
-// });
-
-
 // Mobile Menu
 $(document).ready(function(){
     $(".hamburger-click-area").on("click", function(){
