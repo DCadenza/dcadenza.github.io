facec5224453d9f9c475cb8de68d66a573f85ef0
DCadenza <dimagolubtsov@MacBook-Pro-Dima.local>
2018-03-14 22:11:55 +0200

    Initial commit
    JS and CSS update

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function up() {
     var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
     if(top > 0) {
         window.scrollBy(0,((top+100)/-10));
-        t = setTimeout('up()',30);
+        t = setTimeout('up()',20);
     } else clearTimeout(t);
     return false;
 };
