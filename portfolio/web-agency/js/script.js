993a7f124614877035df402814424fb5eec3d5fc
DMITRII <dmitriiholubtsov@gmail.com>
2018-03-23 13:26:41 +0200

    Global Update
    All Images are minified

---
+++ portfolio/web-agency/js/script.js
@@ -0,0 +1,16 @@
+// Mobile Menu
+$('.js-toggle-menu').click(function(e){
+    e.preventDefault();
+$('.mobile-header-nav').slideToggle();
+$(this).toggleClass('open');
+});
+
+// MAP
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.SATELIT
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
