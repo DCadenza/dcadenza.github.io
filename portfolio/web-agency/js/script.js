cabde37562122e8834b2f94acdeb24fcd36d067b
DMITRII <dmitriiholubtsov@gmail.com>
2018-05-08 16:13:44 +0300

    Initial commit
    Updating map key

--- portfolio/web-agency/js/script.js
+++ portfolio/web-agency/js/script.js
@@ -11,6 +11,7 @@ function myMap() {
         center: new google.maps.LatLng(51.5, -0.12),
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.SATELIT
+        key: AIzaSyAwUwU6ut4w3ylYD-pK_Zcxjs23zq_Q4pI
     }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
