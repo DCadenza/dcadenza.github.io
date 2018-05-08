b07033b5396105ed10be3faf2b4aee97c1133207
DMITRII <dmitriiholubtsov@gmail.com>
2018-05-08 16:47:06 +0300

    Update commit
    increase download speed

--- portfolio/web-agency/js/script.js
+++ portfolio/web-agency/js/script.js
@@ -6,12 +6,12 @@ $(this).toggleClass('open');
 });
 
 // MAP
-function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.SATELIT
-        key: AIzaSyAwUwU6ut4w3ylYD-pK_Zcxjs23zq_Q4pI
-    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
+// function myMap() {
+//     var mapOptions = {
+//         center: new google.maps.LatLng(51.5, -0.12),
+//         zoom: 10,
+//         mapTypeId: google.maps.MapTypeId.SATELIT
+//         key: AIzaSyAwUwU6ut4w3ylYD-pK_Zcxjs23zq_Q4pI
+//     }
+// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+// }
