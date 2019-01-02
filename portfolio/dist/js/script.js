5c1ce5101af3242fb6ec3c876a20bfa32d70ac78
DmitriiHolubtsov <jamie.cadenza@gmail.com>
2019-01-02 10:26:15 +0200

    upd
    upl

--- portfolio/dist/js/script.js
+++ portfolio/dist/js/script.js
@@ -166,51 +166,6 @@ function resizeIfame(frame) {
     });
 };
 
-//Function testimonial video content
-
-//Youtube script
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-
-onYouTubeIframeAPIReady = function () {
-    player = new YT.Player('player', {
-        height: '263',
-        width: '263',
-        videoId: 'AkyQgpqRyBY',  // youtube video id
-        playerVars: {
-            'autoplay': 0,
-            'rel': 0,
-            'showinfo': 0
-        },
-        events: {
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-
-var p = document.getElementById ("player");
-$(p).hide();
-
-var t = document.getElementById ("thumbnail");
-t.src = "../images/anna_image.png";
-
-onPlayerStateChange = function (event) {
-    if (event.data == YT.PlayerState.ENDED) {
-        $('.start-video').fadeIn('normal');
-    }
-}
-
-$(document).on('click', '.start-video', function () {
-    $(this).hide();
-    $("#player").show();
-    $("#thumbnail_container").hide();
-    player.playVideo();
-});
-
 // Function to change table button color on Click
 $('#colorBtn1, #colorBtn2, #colorBtn3').click(function () {
     if (this.id == 'colorBtn1') {
@@ -291,3 +246,22 @@ function showTarget(targets, targetId) {
 }
 
 table()
+
+// Testimonial hide thumbnail function
+
+// video-overlay-1
+document.querySelector(".video-overlay-1").onclick = function(){
+	this.style.display = "none";
+}
+// video-overlay-2
+document.querySelector(".video-overlay-2").onclick = function(){
+	this.style.display = "none";
+}
+// video-overlay-3
+document.querySelector(".video-overlay-3").onclick = function(){
+	this.style.display = "none";
+}
+// video-overlay-4
+document.querySelector(".video-overlay-4").onclick = function(){
+	this.style.display = "none";
+}
