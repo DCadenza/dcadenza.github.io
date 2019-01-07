67d3950c119681136857c0cf83055db0a105ecd6
DmitriiHolubtsov <jamie.cadenza@gmail.com>
2019-01-07 22:40:11 +0200

    Portfolio update
    Frostbite update

--- portfolio/frostbite/js/script.js
+++ portfolio/frostbite/js/script.js
@@ -1,7 +1,31 @@
-$("button.menu").on("click", function() {
-  $(".navigation-container").css("display", "block");
+// Navigation buttons
+$('button.menu').on('click', function() {
+  $('.navigation-container').show('2000');
+  $('button.menu').hide();
+  $('button.search').hide();
+  $('#site-search').hide();
 });
 
-$("button.close").on("click", function(){
-    $(".navigation-container").css("display", "none");
+$('button.close').on('click', function(){
+    $('.navigation-container').hide('2000');
+    $('button.menu').show();
+    $('button.search').show();
+});
+
+// Preloader
+$(window).on('load', function () {
+var $preldr = $('#preloader'),
+    $svg_anm   = $preldr.find('.svg-animation');
+$svg_anm.fadeOut();
+$preldr.delay(1000).fadeOut('slow');
+});
+
+// Search btn and field
+    var open = document.getElementById('site-search');
+    $('button.search').on('click', function(){
+    if(open.style.display == 'none') {
+        $('#site-search').css('display', 'inline-block');
+    } else if (open.style.display == 'inline-block') {
+        $('#site-search').css('display', 'none');
+    }
 });
